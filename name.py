539592a571e1540b4a299cf1d6e71c7c019f8d08
clpiriz74 <clpiriz@gmail.com>
2024-02-11 19:30:47 -0500

    Un programa de manejo de string

---
+++ name.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+
+name="andres perez"
+
+print(name.title())
+
+
+nombre="carlos"
+
+apellido="rodriguez"
+
+
+
+print (f"El nombre es {nombre.upper()} y el apellido {apellido.upper()}")
+
+
+print (f"El nombre es {nombre.upper()} y el apellido {apellido.upper()}")
